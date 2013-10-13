20318856e31af03e6014e272d5becf8e75ad31db
Gabriel Ulloa <gulloa91@gmail.com>
2013-10-13 12:25:23 -0600

    Arreglos de Manuel

---
+++ Proyecta/Controllers/MetasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecta.Controllers
+{
+    public class MetasController : Controller
+    {
+        //
+        // GET: /Metas/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InformacionIDS()
+        {
+            return View();
+        }
+
+    }
+}
